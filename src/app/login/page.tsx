09d9861ae6c8e4529ad8fade5a0485984656d4a6
Heloiusa <heloisacosta2005@gmail.com>
2025-04-27 11:50:35 -0300

    responsividade login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,20 @@
-import ContainerImage from "@/components/containerimage"
-import LoginForm from "@/components/loginform"
-export default function login() {
-    return(
-        <div className = "flex h-screen w-screen "> 
-        {/*lado esquerdo do formulario*/}
-        <div className="w-1/2 h-full ">
-            <ContainerImage />
-        </div>
-        {/*lado esquerdo do formulario*/}   
-            <div className="w-1/2 bg-white h-full flex items-center justify-center">
-                <LoginForm/>
-            </div>
-        </div>
+import ContainerImage from "@/components/containerimage";
+import LoginForm from "@/components/loginform";
 
-   
-    )
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col md:flex-row h-screen w-screen">
+      
+      {/* Lado esquerdo - Imagem (só aparece em telas médias ou maiores) */}
+      <div className="hidden md:flex w-1/2 h-full">
+        <ContainerImage />
+      </div>
 
+      {/* Lado direito - Formulário */}
+      <div className="w-full md:w-1/2 bg-white h-full flex items-center justify-center">
+        <LoginForm />
+      </div>
+
+    </div>
+  );
 }
