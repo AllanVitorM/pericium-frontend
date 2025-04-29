3c27bb3a1ff775889de7b81486d3f75c51f4e602
AllanVitorM <allanvitormarques14@gmail.com>
2025-04-29 08:36:30 -0300

    resolvendo url

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://pericium-backend-production.up.railway.app',
+    baseURL: 'pericium-backend-production.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
